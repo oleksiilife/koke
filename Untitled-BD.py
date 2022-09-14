5f99daabd3d5aaf456aef83874d397e23cde4c77
oleksiilife <113524425+oleksiilife@users.noreply.github.com>
2022-09-14 15:40:17 +0300

    Add files via upload

---
+++ Untitled-BD.py
@@ -0,0 +1,4 @@
+kurs = float(input("What is Bitcoin price today?\n"))
+money = float(input("How moch dollar do you have?\n"))
+bitcoin_price = money/kurs;
+print(f'You can buy {bitcoin_price:.7f} BTC') 
